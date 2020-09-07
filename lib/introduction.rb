49a9b34ef10d6bd6d07f4e4fd94c9501e798222c
peaceful-prototype-4702 <davidamedina619@gmail.com>
2020-09-07 22:43:43 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,7 +2,7 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end 
 
-def introduction_with_language (name,language)
+def introduction_with_language (name="Dan",language="Ember.js")
   puts "Hi, my name is #{name} and i am learning to program in #{language}."
   
 end
